3fea3adfdac6382ceac6d0ab7cc5ce78bfdf12ee
Patricia Trauman <patricia.trauman@loreal.com>
2022-02-19 13:54:13 +0100

    test commit

--- page-booking-activity.php
+++ page-booking-activity.php
@@ -16,7 +16,6 @@
  * Update URI:        https://example.com/my-plugin/
  */
 
-
 if (!function_exists('add_action')) {
 	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
 	exit;
